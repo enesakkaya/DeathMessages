6d901c01aae6410c94bfb02418ad2f835a6f1d49
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-01-21 10:25:15 +1100

    Update DeathMessageListener.java

--- src/com/leetzilantonis/deathmessage/listeners/DeathMessageListener.java
+++ src/com/leetzilantonis/deathmessage/listeners/DeathMessageListener.java
@@ -3,7 +3,6 @@ package com.leetzilantonis.deathmessage.listeners;
 import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -28,8 +27,6 @@ public class DeathMessageListener implements Listener{
 
 	}
 
-
-	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerKill(PlayerDeathEvent e) {
 
@@ -43,24 +40,20 @@ public class DeathMessageListener implements Listener{
 
 			if (killer instanceof Player) {
 
-				ItemStack it = ((Player) killer).getInventory().getItemInHand();
+				plugin.addDeath(e.getEntity());
+				plugin.addKill((Player)killer);
+
+				ItemStack it = ((Player) killer).getItemInHand();
 				String itemName;
 
 				if (it == null || it.getType() == Material.AIR) {
-
 					itemName = plugin.getLangConfig().getString("noItem");
-
 				} else {
-
 					itemName = it.getItemMeta().getDisplayName();
-
 					if (itemName == null) {
-
-						itemName = StringUtils.capitalise(it.getType().toString().replace("_", " ").toLowerCase());
-
+						itemName = it.getType().toString().replace("_", " ").toLowerCase();
 					}
 				}
-				System.out.println(itemName);
 
 				String cause = DeathUtils.getCauseNiceName(p);
 				String attacker = DeathUtils.getAttackerName(p);
@@ -84,8 +77,7 @@ public class DeathMessageListener implements Listener{
 					}
 
 					if(attacker == "pvpwolf"){
-						String owner = DeathUtils.getTameWolfOwner(e);
-						attacker = owner+"'s wolf";
+						attacker = DeathUtils.getTameWolfOwner(e) + "'s wolf";
 					}
 
 					finalMsg = finalMsg.replace("{player}", p.getName());
@@ -93,28 +85,34 @@ public class DeathMessageListener implements Listener{
 					finalMsg = finalMsg.replace("{itemName}", itemName);
 					finalMsg = finalMsg.replace("{item}", "{item}" + ChatColor.RESET.toString());
 
+					String s = plugin.getConfig().getString("stat");
+					if (s.equalsIgnoreCase("deaths")) {
+						finalMsg = finalMsg.replace("{stat}", plugin.getDeaths(p) + "");
+						finalMsg = finalMsg.replace("{killerStat}", plugin.getDeaths((Player)killer) + "");
+					} else if (s.equalsIgnoreCase("kills")) {
+						finalMsg = finalMsg.replace("{stat}", plugin.getKills(p) + "");
+						finalMsg = finalMsg.replace("{killerStat}", plugin.getKills((Player)killer) + "");
+					} else {
+						finalMsg = finalMsg.replace("{stat}", "");
+						finalMsg = finalMsg.replace("{killerStat}", "");
+					}
+
 					finalMsg = ChatColor.translateAlternateColorCodes('&', finalMsg);
 					System.out.println(finalMsg);
-					
+
 					FancyMessage fm = new FancyMessage();
 
 					String[] split = finalMsg.split("\\{item\\}");
-					for (int i = 0; i < split.length; i++) { 
-
+					for (int i = 0; i < split.length; i++) {
 						if (i == split.length - 1) {
-
 							fm.then(split[i]);
-
 						} else {
-
 							String substring = split[i].length() > 2 ? split[i].substring(split[i].length() - 2) : split[i];
 							itemName = substring + itemName;
 							fm.then(split[i])
 							.then(itemName)
 							.itemTooltip(it);
-
 						}
-
 					}
 
 					if(plugin.getConfig().getBoolean("allowCrossWorld") ){
@@ -163,43 +161,37 @@ public class DeathMessageListener implements Listener{
 						finalMsg = (String) messages.get(rand.nextInt(messages.size()));  
 					}
 
-					if(attacker == "pvpwolf"){
-						String owner = DeathUtils.getTameWolfOwner(e);
-						attacker = owner + "'s wolf";
+					if(attacker.equalsIgnoreCase("pvpwolf")){
+						attacker = DeathUtils.getTameWolfOwner(e) + "'s wolf";
 					}
 
 					finalMsg = finalMsg.replace("{player}", p.getName());
 					finalMsg = finalMsg.replace("{killer}", attacker);
 					finalMsg = finalMsg.replace("{mob}", attacker);
 
-					finalMsg = finalMsg.replace("{player}", p.getName());
-					finalMsg = finalMsg.replace("{killer}", attacker);
-					finalMsg = finalMsg.replace("{mob}", attacker);
+					String s = plugin.getConfig().getString("stat");
+
+					if (s.equalsIgnoreCase("deaths")) {
+						finalMsg = finalMsg.replace("{stat}", plugin.getDeaths(p) + "");
+					} else if (s.equalsIgnoreCase("kills")) {
+						finalMsg = finalMsg.replace("{stat}", plugin.getKills(p) + "");
+					} else {
+						finalMsg = finalMsg.replace("{stat}", "");
+					}
 
 					finalMsg = ChatColor.translateAlternateColorCodes('&', finalMsg);
 
-					if(plugin.getConfig().getBoolean("allowCrossWorld")) {
-						if (plugin.hearDistance <= 0) {
-							for (Player player : plugin.getServer().getOnlinePlayers()) {
-								if(player.hasPermission("deathmessages.hear")) {
-									player.sendMessage(finalMsg);
-								}
-							}
-						} else {
-							for (Player player : p.getWorld().getPlayers()) {
-								if(player.hasPermission("deathmessages.hear")){
-									double dist = player.getLocation().distance(p.getLocation());
-									if (dist <= plugin.hearDistance) {
-										player.sendMessage(finalMsg);
-									}
-								}
+					if (plugin.getConfig().getBoolean("allowCrossWorld")) {
+						for (Player player : plugin.getServer().getOnlinePlayers()) {
+							if (player.hasPermission("deathmessages.hear")) {
+								player.sendMessage(finalMsg);
 							}
 						}
 					} else {
 						for (Player player : p.getWorld().getPlayers()) {
-							if(player.hasPermission("deathmessages.hear")){
+							if (player.hasPermission("deathmessages.hear")){
 								double dist = player.getLocation().distance(p.getLocation());
-								if(plugin.getConfig().getDouble("hearDistance") == 0 || dist <= plugin.getConfig().getDouble("hearDistance")) {
+								if (plugin.getConfig().getDouble("hearDistance") == 0 || dist <= plugin.getConfig().getDouble("hearDistance")) {
 									player.sendMessage(finalMsg);
 								}
 							}
